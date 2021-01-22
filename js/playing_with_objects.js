9dbe51bdd2d090752a5e6af9a479ec3c11328774
joshcodingdojo <jgendal@codingdojo.com>
2021-01-21 20:48:13 -0500

    finished playing w objects

---
+++ js/playing_with_objects.js
@@ -0,0 +1,23 @@
+var users = [
+  {
+    name: "Michael",
+    age: 37,
+  },
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "David",
+    age: 27,
+  },
+];
+
+// Log John's age
+console.log(users[1].age);
+// Print name of first object
+console.log(users[0].age);
+// For loop
+for (var i = 0; i < users.length; i++) {
+  console.log(users[i].name);
+}
