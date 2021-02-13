2be29ab456586ee683b2039068fd0baa3c9d7190
joshcodingdojo <jgendal@codingdojo.com>
2021-02-12 22:35:32 -0500

    finished

---
+++ css/HTML_CSS_portfolio_proj/poke_bowl/script.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('yoooo')
+  var contactUs = document.getElementById("contact-us")
+
+  contactUs.addEventListener("click", function() {
+    window.alert("123-456-7890")
+  })
+})
+
